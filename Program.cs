e89f8e18fee4fac0bc29bba54c4268058bc6b205
Oluwadolamipo <dlapsie@gmail.com>
2020-05-07 04:17:48 +0100

    Task

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SmartGuess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        
+    }
+}
