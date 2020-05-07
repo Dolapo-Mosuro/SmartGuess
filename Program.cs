4cb51c94cb173e23206bfe555bdc768629a3f8d9
Oluwadolamipo <dlapsie@gmail.com>
2020-05-07 23:31:59 +0100

    Task 4

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Text;
+using System.Collections;
+using System.Data;
 
 namespace SmartGuess
 {
@@ -6,7 +9,24 @@ namespace SmartGuess
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter a Number : ");
+                int Number = Convert.ToInt16(Console.ReadLine());
+                double SqrtNumber = Math.Sqrt(Number);
+                Console.WriteLine("Square root of {0} is: {1}", Number, SqrtNumber);
+                Console.ReadLine();
+
+
+               if (Number <  0)
+               {
+                  Console.WriteLine ("Error, Number should be more than zero");
+               }
+               else
+               {
+                   Console.WriteLine("Square root of {0} is: {1}", Number, SqrtNumber);
+                Console.ReadLine();
+               }
+
+               
         }
         
     }
